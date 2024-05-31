7de2f797931fb671c45a5d85b99e560598390b64
Nikitka <flgjkdoged228@gmail.com>
2024-05-31 08:31:56 +0300

    update

--- 3.4-django-testing/students/views.py
+++ 3.4-django-testing/students/views.py
@@ -1,9 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
+from students.filters import CourseFilter
 from students.models import Course
 from students.serializers import CourseSerializer
-from students.filters import CourseFilter
 
 class CoursesViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filterset_class = CourseFilter
+    
