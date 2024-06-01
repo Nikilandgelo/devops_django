4accd900fcbc154056a5ff6ddfb86f09bcd02f0b
Nikitka <flgjkdoged228@gmail.com>
2024-06-01 06:17:18 +0300

    in tears trying tests run and see postgresql on port 5432

--- 3.4-django-testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/settings.py
@@ -72,6 +72,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
